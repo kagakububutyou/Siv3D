03064413cf6555e500762d19f6b69062bc0dccfd
kagakububutyou <shinnnosuke1232@yahoo.co.jp>
2014-06-23 11:00:28 +0900

    円を描く

--- Practice/Practice/Main.cpp
+++ Practice/Practice/Main.cpp
@@ -2,22 +2,13 @@
 # include <Siv3D.hpp>
 
 /*
-		ƒJƒ‰ƒtƒ‹‚È‰~
+		‰~‚ð•`‚­
 */
 
 void Main()
 {
-	Graphics::SetBackground(Palette::White);
-
 	while (System::Update())
 	{
-		for (int i = 0; i<36; i++)
-		{
-			const double radian = Radians(i*10.0 + System::FrameCount());
-
-			const Vec2 pos = Vec2(Sin(radian), -Cos(radian)) * 200 + Vec2(320, 240);
-
-			RectF(25, 25).setCenter(pos).rotate(radian).draw(HSV(i * 10));
-		}
+		Circle(300, 200, 150).draw();
 	}
 }
