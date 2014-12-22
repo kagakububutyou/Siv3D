96a5ba22621b4d634283b369799d5bbb37b62557
kagakububutyou <shinnnosuke1232@yahoo.co.jp>
2014-12-22 10:15:19 +0900

    最終調整

--- Dungeon/Dungeon/MenuManager.cpp
+++ Dungeon/Dungeon/MenuManager.cpp
@@ -12,6 +12,7 @@ key(std::make_unique<CMenuKeyEvent>())
 void CMenuManager::Init()
 {
 	TextureAsset::Register(L"OP", L"engine/data/texture/Character/Etc/OP.png");
+	TextureAsset::Register(L"ゲーム説明", L"engine/data/texture/Character/Etc/ゲーム説明.png");
 	//TextureAsset::Register(L"GAMEOVER", L"engine/data/texture/Character/Etc/GAMEOVER.png");
 	Logo = new Font(50, Typeface::Black);
 }
@@ -26,4 +27,5 @@ void CMenuManager::Draw()
 {
 	//Logo->drawCenter(L"メニュー", Float2(CGameApplication::ScreenWidth / 2, 100), Palette::Royalblue);
 	TextureAsset(L"OP")/*.resize(CGameApplication::ScreenWidth, CGameApplication::ScreenHeight)*/.draw();
+	TextureAsset(L"ゲーム説明")/*.resize(CGameApplication::ScreenWidth, CGameApplication::ScreenHeight)*/.draw(CGameApplication::ScreenWidth / 2 - 313, 250);
 }
